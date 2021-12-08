8bc10fa5de412dc1d552cbce40d975caa27bcb31
Soravaki <92048405+Soravaki@users.noreply.github.com>
2021-12-08 12:43:49 -0600

    School Work

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Map;
+import java.util.Scanner;
+import java.io.IOException;
+import java.util.TreeMap;
+
+public class Main {
+    public Main() throws IOException {
+        Scanner m = new Scanner(System.in);
+        System.out.println("Input Sentence:");
+        String s = m.nextLine();
+        Misspelling(s);
+    }
+
+    public void Misspelling(String s) throws IOException {
+        Hashmap();
+        String[] line = s.split(" ");
+        for (int i=0;i<line.length;i++){
+            if (misspellings.containsKey(line[i])){
+
+            }
+        }
+    }
+
+    public void Hashmap() throws IOException {
+        Map<String, String> misspellings = new TreeMap<>();
+        Scanner sc = new Scanner(new File("mispellings.txt"));
+        while (sc.hasNextLine()){
+            String[] words = sc.nextLine().split(" ");
+            misspellings.put(words[0], words[1]);
+        }
+    }
+}
